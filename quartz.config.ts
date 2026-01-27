10849cc2000407091046c573e61c4f30814f6467
Zane601 <2947193496@qq.com>
2026-01-27 21:04:31 +0800

    Quartz sync: Jan 27, 2026, 9:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Brandon",
+    pageTitle: "BrandonXu's Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
